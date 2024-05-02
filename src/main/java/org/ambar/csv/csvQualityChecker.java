41f33d710c9056180856a996136e9655976f1819
Ambar752 <ambar_cat2014@yahoo.in>
2024-05-02 21:53:34 +0100

    Refactor Code to make it more modular

--- src/main/java/org/ambar/csv/csvQualityChecker.java
+++ src/main/java/org/ambar/csv/csvQualityChecker.java
@@ -15,10 +15,38 @@ public class csvQualityChecker {
     private String csvPath;
     private Map<String,validationType> validationMetaData;
 
+    private List<String> columnNamesToBeValidated = new LinkedList<>();
+
+    private Map<String,Integer> columnNamewisePositions = new LinkedHashMap<>();
+
+    private Map<String,List<String>> columnDataStore = new HashMap<>();
+
     public List<List<ValidationUtils.InvalidRecordDetails>> AllResult;
     public csvQualityChecker(String csvPath, Map<String,validationType> validationMetaData) {
         this.csvPath = csvPath;
         this.validationMetaData = validationMetaData;
+    }
+
+    private void saveColumnPositions(String[] currentRow) {
+        int currentColumnPosition = 0;
+        for(String currentColumnValue : currentRow) {
+            this.columnNamewisePositions.put(currentColumnValue,currentColumnPosition);
+            currentColumnPosition++;
+        }
+    }
+    private void setcolumnNamesToBeValidated(Map<String,validationType> validationMetaData) {
+        for(Map.Entry<String,validationType> currValidationMetaData : validationMetaData.entrySet()) {
+            this.columnNamesToBeValidated.add(currValidationMetaData.getKey());
+        }
+    }
+
+    private void setcolumnDataStore(String[] currRecord, List<String> columnNamesToBeValidated) {
+        for(String columnName : columnNamesToBeValidated) {
+            List<String> currColumnData = new LinkedList<>();
+            currColumnData = this.columnDataStore.getOrDefault(columnName,new LinkedList<>());
+            currColumnData.add(currRecord[this.columnNamewisePositions.get(columnName)]);
+            this.columnDataStore.put(columnName,currColumnData);
+        }
     }
 
     public void runCheckers() {
@@ -26,49 +54,27 @@ public class csvQualityChecker {
         List<List<ValidationUtils.InvalidRecordDetails>> Result = new ArrayList<>();
 
         try (CSVReader reader = new CSVReader(new FileReader(this.csvPath))) {
+
             String[] currRecord;
             int currRowPosition    = 0;
-            int currColumnPosition = 0;
 
-            Map<String,Integer> columnNamewisePositions = new LinkedHashMap<>();
-
-            List<String> columnsToBeValidated  = new LinkedList<>();
-            List<String>[] allDataOfAllcolumns = new LinkedList[this.validationMetaData.size()];
+            List<String>[] allDataOfAllcolumns  = new LinkedList[this.validationMetaData.size()];
 
             while ((currRecord = reader.readNext()) != null) {
 
                 if(currRowPosition == 0) {
-
-                    for(Map.Entry<String,validationType> currValidationMetaData : this.validationMetaData.entrySet()) {
-                        columnsToBeValidated.add(currValidationMetaData.getKey());
-                    }
-
-                    for (String value : currRecord) {
-                            columnNamewisePositions.put(value,currColumnPosition);
-                            currColumnPosition++;
-                    }
-
-                    currColumnPosition = 0;
-
-                }
-
-                if(currRowPosition != 0) {
-                    for(int i = 0; i < this.validationMetaData.size(); i++) {
-                        List<String> currColumnData = new LinkedList<>();
-                        if(allDataOfAllcolumns[i] != null) {
-                          currColumnData = allDataOfAllcolumns[i];
-                        }
-                        currColumnData.add(currRecord[columnNamewisePositions.get(columnsToBeValidated.get(i))]);
-                        allDataOfAllcolumns[i] = currColumnData;
-                    }
+                    saveColumnPositions(currRecord);
+                    setcolumnNamesToBeValidated(this.validationMetaData);
+                } else {
+                    setcolumnDataStore(currRecord,this.columnNamesToBeValidated);
                 }
 
                 currRowPosition++;
 
             }
 
-            for(int j=0; j < this.validationMetaData.size(); j++) {
-                ValidationUtils.applyChecks(allDataOfAllcolumns[j],this.validationMetaData.get(columnsToBeValidated.get(j)));
+            for(Map.Entry<String,List<String>> allcolumnData : this.columnDataStore.entrySet()) {
+                ValidationUtils.applyChecks(allcolumnData.getValue(),this.validationMetaData.get(allcolumnData.getKey()));
                 Result.add(ValidationUtils.Result);
             }
 
@@ -107,24 +113,24 @@ public class csvQualityChecker {
         }
     }
 
-//    public static void main(String[] args) throws IOException {
-//        Logger logger = Logger.getLogger("MyLogger");
-//        FileHandler fileHandler = new FileHandler("myLogFile.log",true);
-//        CustomerLogFormatter formatter = new CustomerLogFormatter(CustomerLogFormatter.format.DETAILED);
-//        fileHandler.setFormatter(formatter);
-//        logger.addHandler(fileHandler);
-//
-//        String csvPath = "D:\\ambar\\IntelliJProjects\\csvQualityChecker\\resource\\data\\abc.csv";
-//        Map<String,validationType> checkMetadata = new LinkedHashMap<>();
-//        checkMetadata.put("Col1", validationType.UniqueCheck);
-//        checkMetadata.put("Col3", validationType.NumericCheck);
-//
-//        csvQualityChecker cqHandle = new csvQualityChecker(csvPath,checkMetadata);
-//        cqHandle.runCheckers();
-//        cqHandle.printResults();
-//        logger.info("Completed All Checks Try3");
-//
-//        //List<List<ValidationUtils.InvalidRecordDetails>> Result =  cqHandle.AllResult;
-//        //System.out.println(Result.size());
-//    }
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger("MyLogger");
+        FileHandler fileHandler = new FileHandler("myLogFile.log",true);
+        CustomerLogFormatter formatter = new CustomerLogFormatter(CustomerLogFormatter.format.DETAILED);
+        fileHandler.setFormatter(formatter);
+        logger.addHandler(fileHandler);
+
+        String csvPath = "D:\\ambar\\IntelliJProjects\\csvQualityChecker\\resource\\data\\abc.csv";
+        Map<String,validationType> checkMetadata = new LinkedHashMap<>();
+        checkMetadata.put("Col1", validationType.UniqueCheck);
+        checkMetadata.put("Col3", validationType.NumericCheck);
+
+        csvQualityChecker cqHandle = new csvQualityChecker(csvPath,checkMetadata);
+        cqHandle.runCheckers();
+        cqHandle.printResults();
+        logger.info("Completed All Checks Try3");
+
+        //List<List<ValidationUtils.InvalidRecordDetails>> Result =  cqHandle.AllResult;
+        //System.out.println(Result.size());
+    }
 }
